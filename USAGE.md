c64c60793af8b88488a89f1c3defa8a7fbcc76a1
Matt Haggard <haggardii@gmail.com>
2018-07-24 11:02:25 -0600

    python seems to work

--- USAGE.md
+++ USAGE.md
@@ -6,18 +6,22 @@ Opts:
 
     --start-time=   HH:MM when this program should be running
     --stop-time=    HH:MM when this program should not be running
-    --stdout=       File to write subcommand's stdout to.  If not
+    --stdout=       File to append subcommand's stdout to.  If not
                     given, then write to this process' stdout.
-    --stderr=       file to write subcommand's stderr to.  If not
+    --stderr=       file to append subcommand's stderr to.  If not
                     given, then write to this process' stderr.
 
 --start-time and --stop-time define a window during which the subcommand should be running.  Running this program will ensure that COMMAND [ARG...] is started during the timewindow and stopped when outside the timewindow.
 
+--start-time and --stop-time are specified in 24-Hour format in the UTC timezone.
+
 If neither --start-time nor --stop-time is given, COMMAND is run immediately and continues until completion.
+
+If --start-time and --stop-time are the same, COMMAND is run immediately and continues until completion (as if neither option were given).
 
 The program fails to run if only one of --start-time or --stop-time is given.
 
-This program exits with the same return code that COMMAND returns.
+This program exits with the same return code that COMMAND returns as soon as COMMAND exits.
 
 Environment variables provided to this command are provided to COMMAND.
 
--- python/timewindow.py
+++ python/timewindow.py
@@ -2,15 +2,147 @@
 from __future__ import print_function
 import subprocess
 import click
+import signal
+import os
+import sys
+import time
+from datetime import datetime
+
+def log(*x):
+    prefix = '[timewindow] {0} '.format(datetime.now().isoformat())
+    sys.stderr.write(prefix + ' '.join(map(str, x)) + '\n')
+    sys.stderr.flush()
+
+def nowTime():
+    return parseTime(datetime.utcnow().strftime('%H:%M'))
+
+def parseTime(x):
+    h, m = x.split(':')
+    h = int(h, 10)
+    m = int(m, 10)
+    return (h,m)
+
+SECONDS_IN_DAY = 24 * 60 * 60
+
+def timeToSeconds((h,m)):
+    return (m * 60) + (h * 60 * 60)
+
+
+def inStartWindow(start, stop):
+    if not start and not stop:
+        return True
+    now = nowTime()
+    if start > stop:
+        # ---|   |---  run-time spans midnight
+        if now >= start:
+            # ---|   |-X-
+            return True
+        elif now < stop:
+            # -X-|   |---
+            return True
+        else:
+            # ---| X |---
+            return False
+    else:
+        #    |---|     run-time fits in a single day
+        return start <= now < stop
+
+def secondsToNextEvent(start, stop):
+    if not start and not stop:
+        return 1
+    now = nowTime()
+    now_seconds = timeToSeconds(now) + int(datetime.utcnow().strftime('%S'))
+    print('now', now, 'start', start, 'stop', stop)
+    if start > stop:
+        # ---|   |---  run-time spans midnight
+        if now >= start:
+            # ---|   |-X-
+            return SECONDS_IN_DAY - now_seconds + timeToSeconds(stop) + 1
+        elif now < stop:
+            # -X-|   |---
+            return timeToSeconds(stop) - now_seconds + 1
+        else:
+            # ---| X |---
+            return timeToSeconds(start) - now_seconds + 1
+    else:
+        #    |---|     run-time fits in a single day
+        if start <= now < stop:
+            #    |-X-|
+            return timeToSeconds(stop) - now_seconds + 1
+        elif now >= stop:
+            #    |---| X 
+            return SECONDS_IN_DAY - now_seconds + timeToSeconds(start) + 1
+        else:
+            #  X |---|
+            return timeToSeconds(start) - now_seconds + 1
+
+def executeInTimewindow(args, start, stop, stdout, stderr):
+    p = None
+    if stdout:
+        stdout = open(stdout, 'a+')
+    if stderr:
+        stderr = open(stderr, 'a+')
+    is_paused = False
+    while True:
+        if p is None:
+            # not started yet
+            if inStartWindow(start, stop):
+                log('starting')
+                p = subprocess.Popen(args, env=os.environ, stdout=stdout, stderr=stderr, stdin=subprocess.PIPE)
+                # p.stdin.close()
+            else:
+                wait = secondsToNextEvent(start, stop)
+                log('waiting {0}s to start'.format(wait))
+                time.sleep(wait)
+        else:
+            # started
+            if p.poll() is None:
+                # not yet finished
+                if inStartWindow(start, stop):
+                    if is_paused:
+                        # resume it
+                        log('resuming (SIGCONT)')
+                        p.send_signal(signal.SIGCONT)
+                        is_paused = False
+                    else:
+                        # keep running
+                        # XXX it would be nice to use something like select here instead of polling.
+                        time.sleep(1)
+                else:
+                    # pause it
+                    wait = secondsToNextEvent(start, stop)
+                    log('pausing (SIGSTOP) for {0}s'.format(wait))
+                    p.send_signal(signal.SIGSTOP)
+                    is_paused = True
+                    time.sleep(wait)
+            else:
+                # finished
+                log('finished ({0})'.format(p.returncode))
+                sys.exit(p.returncode)
+
+def optionalTime(x):
+    if not x:
+        return None
+    else:
+        return parseTime(x)
 
 @click.command()
-@click.option('--start-time')
-@click.option('--stop-time')
+@click.option('--start-time', type=optionalTime)
+@click.option('--stop-time', type=optionalTime)
 @click.option('--stdout')
 @click.option('--stderr')
 @click.argument('args', nargs=-1, required=True)
 def main(start_time, stop_time, stdout, stderr, args):
-    print('hey', args)
+    if start_time == stop_time:
+        start_time = None
+        stop_time = None
+    if (start_time or stop_time) and not (start_time and stop_time):
+        raise Exception('You must either specify both start-time and stop-time or use neither.')
+    executeInTimewindow(args,
+        start=start_time,
+        stop=stop_time,
+        stdout=stdout,
+        stderr=stderr)
 
 if __name__ == '__main__':
     main()
